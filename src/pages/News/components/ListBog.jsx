243c1da3f917a141068e25b73d5c9a80147e2cbc
unknown <anhcuonggt@gmail.com>
2022-06-20 16:50:35 +0700

    update

--- src/pages/News/components/ListBog.jsx
+++ src/pages/News/components/ListBog.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from "react";
-import homeApi from "../../../api/homeApi";
 import BlogItem from "./BlogItem";
 
 function ListBog({ blogList }) {
-  console.log(blogList);
   return (
     <div className="list-blogs">
       {blogList?.map((item) => {
